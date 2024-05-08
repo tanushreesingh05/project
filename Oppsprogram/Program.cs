017521f186f714125280b21fe9ef523f5e107036
tanushreesingh05 <tanushreesingh555@gmail.com>
2024-05-08 11:44:22 +0530

    [tanushree] added single-inheritance code

--- Oppsprogram/Program.cs
+++ Oppsprogram/Program.cs
@@ -2,11 +2,28 @@
 
 namespace Oppsprogram
 {
+    //single inheritance example
+    public class Vehicle
+    {
+
+        public string brand = "Ford";
+        public void hork()
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+
+    }
+    class Car : Vehicle { 
+        public string modelName = "Mustang";  
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car c = new Car();
+            c.hork();
+
+            Console.WriteLine(c.brand + " " + c.modelName);
         }
     }
 }
