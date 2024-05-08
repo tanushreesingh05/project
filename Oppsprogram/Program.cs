8f13ffa16dffa649ae566fb772dbea16e10eafdf
tanushreesingh05 <tanushreesingh555@gmail.com>
2024-05-08 13:20:47 +0530

    [tanushree] added anonymous function 1.lambda expression - code

--- Oppsprogram/Program.cs
+++ Oppsprogram/Program.cs
@@ -2,55 +2,18 @@
 
 namespace Oppsprogram
 {
-    //Program of aggregation
-    
-    public class Address
-    {
-        public string addressLine;
-        public string city;
-        public string state;
+    //Program of Anonymous function : 1. Lambda expression
 
-        public Address(string addressLine, string city, string state)
-        {
-              this.addressLine = addressLine;
-            this.city = city;
-            this.state = state;
-
-
-        }
-    }
-    
-    public class Employee
-    {
-        public int id;
-        public string name;
-        public Address address;
-
-        public Employee(int id, string name, Address address)
-        {
-            this.id = id;
-            this.name = name;
-            this.address = address;
-        }
-
-        public void display()
-        {
-            Console.WriteLine("Employee ID : " + id);
-            Console.WriteLine("Employee Name : " + name); 
-            Console.WriteLine("Employee Address : " + address.addressLine + " " + address.city + " " + address.state);
-       
-             
-        }
-    }
-
-
+    //
+    delegate int Addition(int a, int b);
     class Program
     {
         static void Main(string[] args)
         {
-            Address a = new Address("P-32", "Thane", "Maharashtra");
-            Employee e = new Employee(101, "Taushree singh", a);
-            e.display();
+            Addition add = ((x, y) => x + y);
+            int result = add(4, 8);
+
+            Console.WriteLine("Addition: " + result); // Output: Addition: 12
         }
     }
 }
