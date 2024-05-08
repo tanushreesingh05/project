b1fc99b239cfd7c1be9fdfd5a146d7c1a2f579b0
tanushreesingh05 <tanushreesingh555@gmail.com>
2024-05-08 10:49:57 +0530

    added

---
+++ Oppsprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oppsprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
