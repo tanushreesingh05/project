27ab1621fa6344b96bfb84e965bf3854d5cf90a2
tanushreesingh05 <tanushreesingh555@gmail.com>
2024-05-08 15:01:01 +0530

    [tanushree] added DS-Arrays Code

--- Oppsprogram/Program.cs
+++ Oppsprogram/Program.cs
@@ -2,18 +2,38 @@
 
 namespace Oppsprogram
 {
-    //Program of Anonymous function : 1. Lambda expression
-
-    //
-    delegate int Addition(int a, int b);
+    //Program of array data structure
     class Program
     {
         static void Main(string[] args)
         {
-            Addition add = ((x, y) => x + y);
-            int result = add(4, 8);
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
 
-            Console.WriteLine("Addition: " + result); // Output: Addition: 12
+            //accessing the element 
+            Console.WriteLine("Accessing arr value using for loop: ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine("Accessing arr value using foreach loop");
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i+ " ");
+            }
+            Console.WriteLine("Acessing arr value using while loop");
+            int j = 0;
+            while (j < arr.Length)
+            {
+                Console.WriteLine(arr[j] + " ");
+                j++;
+            } 
+            Console.WriteLine("Acessing arr value using do - while loop");
+            int k = 0;
+            do
+            {
+                Console.WriteLine(arr[k] + " ");
+                k++;
+            } while (k < arr.Length);
         }
     }
 }
