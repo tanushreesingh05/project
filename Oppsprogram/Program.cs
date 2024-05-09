f2ceb4c81915bcb01888b82f37b6a72b0fc92486
tanushreesingh05 <tanushreesingh555@gmail.com>
2024-05-09 13:56:44 +0530

    [tanushree] added DS-Queue implementation using array code

--- Oppsprogram/Program.cs
+++ Oppsprogram/Program.cs
@@ -1,82 +1,95 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
+
+//Program of Queue implementation using array
 namespace Oppsprogram
 {
-    // Program of stack DS implementation using LinkedList
-
-   public  class Node
+   class Queue
     {
-      public int data;
-       public Node next;
+        int[] arr;
+        int size;
+        int rear=-1;
+        int front = 0;
 
-        public Node(int data)
+       public Queue(int size)
         {
-            this.data = data;
-            next = null;
+            arr = new int[size];
+            this.size = size;
         }
-    }
-    class MyStack
-    {
-        Node head;
 
         public bool IsEmpty()
         {
-            return head == null;
+            return rear == -1;
         }
-       
-
-        public void Push(int data)
+        //enqueue
+        public void add(int data)
         {
-            Node newNode = new Node(data);
-            if (IsEmpty())
+            if (rear == arr.Length - 1)
             {
-                head= newNode;
+                Console.WriteLine("Stack is Full");
                 return;
-                
             }
-
-            newNode.next = head;
-            head = newNode; 
-
+            rear++;
+            arr[rear] = data;
         }
-        public int Pop()
-        {
-            if (IsEmpty())
-            {
-                return -1;
-            }
-            int top = head.data;
-            head = head.next;
-            return top;
-        }
-        public int Peek()
-        {
-            if (IsEmpty())
-            {
-                return -1;
-            }
-            return head.data;
-            
-        }
-
        
+
+        public int remove()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+            int removeditem = arr[front];
+            front++;
+           
+           
+            
+            return removeditem;
+        }
+
+        public int peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+
+            return arr[front];
+        }
     }
+
+    
+    
 
     class Program
     {
         static void Main(string[] args)
         {
-            MyStack s = new MyStack();
 
-            s.Push(10);
-            s.Push(20);
-            s.Push(30);
+            Queue q = new Queue(5);
+            q.add(1);
+            q.add(2);
+            q.add(3);
+            q.add(4);
+            q.add(5);
 
-            while (!s.IsEmpty())
+            Console.WriteLine("Front value of queue : " + q.peek());
+
+            while (!q.IsEmpty())
             {
-                Console.WriteLine(s.Peek());
-                s.Pop();
-            } 
+                Console.WriteLine(q.peek());
+                q.remove();
+            }
+
+
+
+
+
         }
     }
 }
